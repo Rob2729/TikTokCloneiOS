4bc88d611192bd5f8829f7ce3664b8430a3eae50
Rob2729 <Rob.niv@hotmail.com>
2022-02-28 17:51:42 +0000

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Robert on 28/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
